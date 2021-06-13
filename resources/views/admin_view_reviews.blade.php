784230463a45c4fb2537454c144502e2ee63030d
priya1927 <mca.1927@unigoa.ac.in>
2021-06-13 21:13:15 +0530

    added message for no bookings and no reviews

--- resources/views/admin_view_reviews.blade.php
+++ resources/views/admin_view_reviews.blade.php
@@ -10,10 +10,16 @@
 	}
 </style>
 <h2 class="text-center" style="text-shadow: 2px 2px;">Reviews</h2>
-@foreach($reviews as $key => $rev)
-<div class="shadow p-3 mb-5 rounded rev">
-	<div class="d-inline-block"><span class="font-weight-bold"> Name:</span> {{ $events[$key]->fullname }} </div>
-	<div class="d-inline-block" style="text-indent: 50px;"><span class="font-weight-bold">Event:</span> {{ $events[$key]->event_type }} </div>
-	<p class="pstyle"><span class="font-weight-bold">Review:</span> {{ $rev['review'] }}</p>
-</div>
+@if($reviews==null)
+	<div class="jumbotron m-5">
+	  <h3 class="text-center">There are no reviews yet!</h3>
+	</div>
+@else if
+	@foreach($reviews as $key => $rev)
+	<div class="shadow p-3 mb-5 rounded rev">
+		<div class="d-inline-block"><span class="font-weight-bold"> Name:</span> {{ $events[$key]->fullname }} </div>
+		<div class="d-inline-block" style="text-indent: 50px;"><span class="font-weight-bold">Event:</span> {{ $events[$key]->event_type }} </div>
+		<p class="pstyle"><span class="font-weight-bold">Review:</span> {{ $rev['review'] }}</p>
+	</div>
 @endforeach
+@endif
--- resources/views/view_bookings.blade.php
+++ resources/views/view_bookings.blade.php
@@ -13,16 +13,22 @@
 	}
 </style>
 <h2 class="text-center" style="text-shadow: 2px 2px;">My Bookings</h2>
-@foreach($bookings as  $key => $booking)
-<a href="view_booking_details?bkgId={{ $booking['id'] }}">
-	<div class="shadow p-3 mb-5 rounded rev">
-		<p class="d-inline-block pstyle"><span class="font-weight-bold">Name:</span> {{ $events[$key]->fullname }} </p>
-		<p class="d-inline-block pstyle"><span class="font-weight-bold">Event:</span> {{ $events[$key]->event_type }} </p>
-		<p class="d-inline-block pstyle"><span class="font-weight-bold"> Date: </span> {{ $events[$key]->event_date }}</p>
-		<p class="d-inline-block pstyle"><span class="font-weight-bold"> Time: </span> {{ $events[$key]->event_time }} </p>
-		<p class="d-inline-block pstyle"><span class="font-weight-bold"> Booking status: </span> {{ $booking['booking_status'] }} </p>
-		<p class="d-inline-block pstyle"><span class="font-weight-bold"> No of guests: </span> {{ $booking['no_of_guests'] }} </p>
+@if ($bookings->isEmpty())
+	<div class="jumbotron" style="margin: 5%;">
+	  <h3 class="text-center">You haven't made any bookings yet!</h3>
 	</div>
-</a>
-@endforeach
+@else if
+	@foreach($bookings as  $key => $booking)
+	<a href="view_booking_details?bkgId={{ $booking['id'] }}">
+		<div class="shadow p-3 mb-5 rounded rev">
+			<p class="d-inline-block pstyle"><span class="font-weight-bold">Name:</span> {{ $events[$key]->fullname }} </p>
+			<p class="d-inline-block pstyle"><span class="font-weight-bold">Event:</span> {{ $events[$key]->event_type }} </p>
+			<p class="d-inline-block pstyle"><span class="font-weight-bold"> Date: </span> {{ $events[$key]->event_date }}</p>
+			<p class="d-inline-block pstyle"><span class="font-weight-bold"> Time: </span> {{ $events[$key]->event_time }} </p>
+			<p class="d-inline-block pstyle"><span class="font-weight-bold"> Booking status: </span> {{ $booking['booking_status'] }} </p>
+			<p class="d-inline-block pstyle"><span class="font-weight-bold"> No of guests: </span> {{ $booking['no_of_guests'] }} </p>
+		</div>
+	</a>
+	@endforeach
+@endif
 @endsection
